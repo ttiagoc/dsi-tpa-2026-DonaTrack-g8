0579dca812f0b5a74557b19c12c34dc478965983
Tomas Goldszmidt <tgoldszmidt@frba.utn.edu.ar>
2026-06-18 18:17:24 -0300

    Se corrige estructura del repo

--- logistica/src/main/java/ar/edu/utn/frba/ddsi/logistica/LogisticaServiceApplication.java
+++ logistica/src/main/java/ar/edu/utn/frba/ddsi/logistica/LogisticaServiceApplication.java
@@ -1,4 +1,4 @@
-package ar.edu.utn.frba.ddsi.donaciones;
+package ar.edu.utn.frba.ddsi.logistica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- logistica/src/test/java/ar/edu/utn/frba/ddsi/logstica/LogisticaServiceApplicationTests.java
+++ logistica/src/test/java/ar/edu/utn/frba/ddsi/logstica/LogisticaServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package ar.edu.utn.frba.ddsi.donaciones;
+package ar.edu.utn.frba.ddsi.logistica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
